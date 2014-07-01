142b346b47ac798b5fd4721cadc57b7d1daec454
Ben Karl <benrkarl@gmail.com>
2014-06-30 21:07:54 -0400

    add actions for update delete and new along with corresponding views

--- app.rb
+++ app.rb
@@ -7,3 +7,34 @@ get '/' do
   @to_dos = To_do.all
   erb :index
 end
+
+get '/to_dos/new' do
+  erb :new
+end
+
+post '/to_dos' do
+  to_do = To_do.create({
+    description: params[:description],
+    due_date: params[:due_date]
+    })
+  redirect '/'
+end
+
+get '/to_dos/:id/edit' do
+  @to_do = To_do.find(params[:id])
+  erb :edit
+end
+
+put '/to_dos/:id' do
+  to_do = To_do.find(params[:id])
+  to_do.description = params[:description]
+  to_do.due_date = params[:due_date]
+  to_do.save
+  redirect '/'
+end
+
+delete '/to_dos/:id' do
+  to_do = To_do.find(params[:id])
+  to_do.delete
+  redirect '/'
+end
